a4c353767d0c5c1295604f4e2826ea1b65f444e2
Guanghao Huang <76604805+Haha80680@users.noreply.github.com>
2024-03-24 17:48:19 -0400

    Update Team.java

--- src/main/java/soccerteam/Team.java
+++ src/main/java/soccerteam/Team.java
@@ -17,7 +17,7 @@ public class Team {
       throw new IllegalArgumentException("A team must have at least 10 players");
     }
     if (players.size() > 20) {
-      players.sort(Comparator.comparingInt(Player::getSkillLevel));
+      players.sort(Comparator.comparingInt(Player::getSkillLevel).reversed());
       this.players = players.subList(0, 20);
     } else {
       this.players = players;
