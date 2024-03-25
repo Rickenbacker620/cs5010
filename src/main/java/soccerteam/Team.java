7eb1691f5fcd23bdd628eb2d5a3dc7c533b01cb5
Rickenbacker620 <fu78sion@gmail.com>
2024-03-25 14:06:32 -0400

    rewrote selectStartingLineup method

--- src/main/java/soccerteam/Team.java
+++ src/main/java/soccerteam/Team.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
@@ -16,17 +17,21 @@ public class Team {
   private List<Player> players = new ArrayList<>();
 
   /**
-   * Constructor of the class. Initialize the size and players. Throw exception when the size is
+   * Constructor of the class. Initialize the size and players. Throw exception
+   * when the size is
    * invalid
-   * @param size input size of the team
+   *
+   * @param size    input size of the team
    * @param players input players of the team
-   * @throws IllegalArgumentException when size is smaller than 10 and bigger than 20
+   * @throws IllegalArgumentException when size is smaller than 10 and bigger than
+   *                                  20
    */
   public Team(int size, List<Player> players) throws IllegalArgumentException {
     if (players.size() < 10) {
       throw new IllegalArgumentException("A team must have at least 10 players");
     }
     if (players.size() > 20) {
+      // lowest skill level players are removed if team size is bigger than 20
       players.sort(Comparator.comparingInt(Player::getSkillLevel).reversed());
       this.players = players.subList(0, 20);
     } else {
@@ -36,6 +41,7 @@ public class Team {
 
   /**
    * Method to add one player into the team
+   *
    * @param player input player to be added
    */
   public void addPlayer(Player player) {
@@ -51,7 +57,7 @@ public class Team {
    * Method to randomly generate jersey number for players
    */
   public void generateJerseyNumber() {
-    Set<Integer> assignJerseyNumbers=new HashSet<>();
+    Set<Integer> assignJerseyNumbers = new HashSet<>();
     Random random = new Random();
     for (Player player : players) {
       int jerseyNumber;
@@ -63,44 +69,74 @@ public class Team {
   }
 
   /**
-   * Method to generate the starting line players. Compare the players in the team by their skill
+   * Method to generate the starting line players. Compare the players in the team
+   * by their skill
    * level. Put the players with better skill level to the line
    */
-  public void selectStartingLineup(){
+  public void selectStartingLineup() {
     activePlayers.clear();
     benchPlayers.clear();
     players.sort(Comparator.comparingInt(Player::getSkillLevel).reversed());
-    for (int i=0;i<players.size();i++){
-      if(i<7&&activePlayers.size()<7){
-        activePlayers.add(players.get(i));
+
+    Map<Position, Integer> positionCount = Map.of(
+        Position.GOALIE, 1,
+        Position.DEFENDER, 2,
+        Position.MIDFIELDER, 2,
+        Position.FORWARD, 2);
+
+    for (int i = 0; i < players.size(); i++) {
+      Player currentPlayer = players.get(i);
+      Position position = currentPlayer.getPreferredPosition();
+      // get the remaining position count
+      int remainingPositionCount = positionCount.get(position);
+
+      if (remainingPositionCount > 0) {
+        // put player in the starting line if there is still position available
+        activePlayers.add(currentPlayer);
+        positionCount.put(position, remainingPositionCount - 1);
       } else {
-        benchPlayers.add(players.get(i));
+        // put player in the bench if there is no position available
+        benchPlayers.add(currentPlayer);
       }
     }
+
+    // fill the remaining position in the starting line with the highest skill level
+    int unfilledPositions = 7 - activePlayers.size();
+    for (int i = 0; i < unfilledPositions; i++) {
+      activePlayers.add(benchPlayers.removeFirst());
+    }
+    players.sort(Comparator.comparing(Player::getLastName));
+    activePlayers.sort(Comparator.comparing(Player::getLastName));
   }
 
   /**
-   * Method to generate the list of players in string. In format of a space in between first name,
+   * Method to generate the list of players in string. In format of a space in
+   * between first name,
    * last name, jersey number and a new line after each player
+   *
    * @return string of players in the team
    */
-  public String getPlayerList(){
+  public String getPlayerList() {
     StringBuilder playerList = new StringBuilder();
     for (Player player : players) {
-      playerList.append(player.getFirstName()).append(" ").append(player.getLastName()).append(" ").append(player.getJerseyNumber()).append("\n");
+      playerList.append(player.getFirstName()).append(" ").append(player.getLastName()).append(" ")
+          .append(player.getJerseyNumber()).append("\n");
     }
     return playerList.toString();
   }
 
   /**
-   * Method to generate the list of start line players in string. In format of a space in between
+   * Method to generate the list of start line players in string. In format of a
+   * space in between
    * first name, last name, jersey number and a new line after each player
+   *
    * @return string of start line players in the team
    */
-  public String getStartingLineup(){
+  public String getStartingLineup() {
     StringBuilder playerList = new StringBuilder();
     for (Player player : activePlayers) {
-      playerList.append(player.getFirstName()).append(" ").append(player.getLastName()).append(" ").append(player.getJerseyNumber()).append("\n");
+      playerList.append(player.getFirstName()).append(" ").append(player.getLastName()).append(" ")
+          .append(player.getJerseyNumber()).append("\n");
     }
     return playerList.toString();
   }
